8109376924aa534aeac4b14e14e2ec7870e985c7
duchuy188 <95694503+duchuy188@users.noreply.github.com>
2024-11-04 13:38:19 +0700

    Update and rename main.jsx to main.js

--- src/main.js
+++ src/main.js
@@ -1,21 +1,20 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-import App from './App.jsx'
+import App from './App'
 import './index.css'
 import { ToastContainer } from 'react-toastify'
 import 'react-toastify/dist/ReactToastify.css';
 import { Provider } from "react-redux";
-import { persistor, store } from "./components/redux/store.js";
+import { persistor, store } from "./components/redux/store";
 import { PersistGate } from "redux-persist/integration/react";
+
 createRoot(document.getElementById('root')).render(
-  
-    <>
+  <>
     <Provider store={store}>
       <PersistGate loading={null} persistor={persistor}>
         <ToastContainer />
         <App />
       </PersistGate>
     </Provider>
-    </>
-
-)
+  </>
+) 

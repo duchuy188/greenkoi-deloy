86c4bad31c4084a73b94aa96b11a3cb2f00b30ae
duchuy188 <95694503+duchuy188@users.noreply.github.com>
2024-11-04 13:52:33 +0700

    Update vite.config.js

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,17 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
+  esbuild: {
+    loader: "jsx",
+    include: /src\/.*\.jsx?$/,
+  },
+  optimizeDeps: {
+    esbuild: {
+      loader: {
+        '.js': 'jsx',
+      },
+    },
+  },
   server: {
     port: 5174,
     proxy: {
